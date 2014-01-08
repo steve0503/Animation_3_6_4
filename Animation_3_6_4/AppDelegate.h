79f7a8d5c00e8e1524c409abca02dadbe25d76fe
steve0503 <leehr76@gmail.com>
2014-01-09 07:16:30 +0900

    Initial Commit

---
+++ Animation_3_6_4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Animation_3_6_4
+//
+//  Created by 이 해용 on 2014. 1. 9..
+//  Copyright (c) 2014년 iamdreamer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Animation_3_6_4/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Animation_3_6_4
+//
+//  Created by 이 해용 on 2014. 1. 9..
+//  Copyright (c) 2014년 iamdreamer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
